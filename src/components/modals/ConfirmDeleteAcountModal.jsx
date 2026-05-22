b3eae5365ca75251ea1f38c2b6a68eee5bdaadf9
Gustavo Carvalho <gch1412@proton.me>
2026-05-22 12:38:13 -0300

    Error message updated

--- src/components/modals/ConfirmDeleteAcountModal.jsx
+++ src/components/modals/ConfirmDeleteAcountModal.jsx
@@ -7,12 +7,11 @@ const ConfirmDeleteAccountModal = ({ showModal, setShowModal, isSuccess, handleD
 
   return (
     <Modal show={showModal && isSuccess}>
-      {isError ? <p className="fs-4 err_msg">{error.data}</p> : null}
-      
       <Modal.Header>
         <h2>Confirmar</h2>
       </Modal.Header>
       <Modal.Body className="fs-5">
+        {isError ? <p className="fs-4 err_msg">{error.data}</p> : null}
         <p>Você tem certeza que deseja deletar sua conta? Essa operação não pode ser desfeita</p>
       </Modal.Body>
       <Modal.Footer>
